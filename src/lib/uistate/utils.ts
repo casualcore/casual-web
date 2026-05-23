ed8ae06270d6df69e2f27bf3e90b769158efe050
Stefan Burman Lundin <>
2026-05-24 00:55:50 +0200

    Use iso8601 UTC offset format

--- src/lib/uistate/utils.ts
+++ src/lib/uistate/utils.ts
@@ -16,8 +16,10 @@ export const tableUtils = (): TableUtils => {
   const formatDuration = (time: number): string =>
     (time / 1000000000).toFixed(DURATIONS_RESOLUTION_DECIMALS[uiState.durationsResolution]);
 
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
   const formatDate = (date: number): string =>
-    date > 0 ? new Date(date / 1000000).toISOString() : "";
+    date > 0 ? toIsoOffset(new Date(date / 1000000), timezone) : "";
 
   return {
     hiddenBy,
@@ -25,3 +27,36 @@ export const tableUtils = (): TableUtils => {
     formatDate,
   };
 };
+
+export function toIsoOffset(
+  date: Date = new Date(),
+  timeZone: string = 'UTC',
+) {
+  const parts = new Intl.DateTimeFormat('en', {
+    timeZone,
+    hour12: false,
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZoneName: 'longOffset',
+  })
+    .formatToParts(date)
+    .reduce<Record<string, string>>((acc, part) => {
+      if (part.type !== 'literal') {
+        acc[part.type] = part.value;
+      }
+
+      return acc;
+    }, {});
+
+  const offset = parts.timeZoneName.replace('GMT', '');
+
+  return (
+    `${parts.year}-${parts.month}-${parts.day}` +
+    `T${parts.hour}:${parts.minute}:${parts.second}` +
+    offset
+  );
+}
